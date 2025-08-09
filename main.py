e644ddf94a59fe11600f12544d9d037f557bb69c
João Lucas Rodrigues <108086840+JoaoLucasRodriguesdaSilva@users.noreply.github.com>
2025-08-09 17:58:11 +0000

    Main application

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # Adiciona o diretório raiz do projeto ao path para permitir importações de módulos
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), )))
 
 from database.criar_bd import criar_db
 from database.receitas import (
