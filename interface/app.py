a88bd8c30caefd1d0c4f432a48bb2c8206a99307
João Lucas Rodrigues <108086840+JoaoLucasRodriguesdaSilva@users.noreply.github.com>
2025-08-10 20:08:40 +0000

    fix

--- interface/app.py
+++ interface/app.py
@@ -1,6 +1,13 @@
+import sys
+import os
 import tkinter as tk
 from tkinter import ttk
 from Receitas.receitas import ReceitasFrame
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from database.criar_bd import create_table
+
+create_table()
 
 # --- Main Application Window ---
 root = tk.Tk()
