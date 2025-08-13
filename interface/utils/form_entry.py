f8abed2691aa29bb5d52acecfc9a627624046076
João Lucas Rodrigues <108086840+JoaoLucasRodriguesdaSilva@users.noreply.github.com>
2025-08-13 14:57:53 +0000

    Ajustando popup 4

--- interface/utils/form_entry.py
+++ interface/utils/form_entry.py
@@ -7,7 +7,7 @@ class FormEntry:
         self.entry = ttk.Entry(self.frame, width=width)
         self.entry.insert(0, placeholder)
         self.label.grid(row=0, column=0, padx=5, sticky="w")
-        self.entry.grid(row=1, column=0, padx=5, pady=5)
+        self.entry.grid(row=1, column=0, padx=5)
 
     def get_entry_value(self):
         return self.entry.get()
